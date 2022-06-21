5d613cff8e7084dd7bbd940704ed6519b084668c
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2022-06-21 02:17:00 -0600

    Polished concurrent-map and added tests

--- datastructures/concurrent-map.go
+++ datastructures/concurrent-map.go
@@ -3,11 +3,115 @@ package datastructures
 import (
 	"bytes"
 	"encoding/gob"
-	"fmt"
 	"sync"
 )
 
-const shardCount = 128
+const ShardCount = 128
+
+// ConcurrentMap is a sharded, thread-safe mapping of keys to values.
+type concurrentMap[KEY comparable, VAL any] struct {
+	shards [ShardCount]*shard[KEY, VAL]
+}
+
+// NewConcurrentMap creates a new ConcurrentMap.
+func NewConcurrentMap[KEY comparable, VAL any]() *concurrentMap[KEY, VAL] {
+	c := concurrentMap[KEY, VAL]{
+		shards: [ShardCount]*shard[KEY, VAL]{},
+	}
+	for i := range c.shards {
+		c.shards[i] = newShard[KEY, VAL]()
+	}
+	return &c
+}
+
+func (c concurrentMap[KEY, VAL]) getShard(key KEY) *shard[KEY, VAL] {
+	return c.shards[fnv1a(key)%uint64(ShardCount)]
+}
+
+// At returns the value for the given key and a bool indicating whether or not
+// the given key exists in the map. If the given key does not exist, then the
+// zero-value of VAL and false are returned.
+func (c *concurrentMap[KEY, VAL]) At(key KEY) (VAL, bool) {
+	shard := c.getShard(key)
+	return shard.at(key)
+}
+
+// Set inserts the given val at the specified key, or overrides the
+// current value for that key if there is one.
+func (c *concurrentMap[KEY, VAL]) Set(key KEY, val VAL) {
+	shard := c.getShard(key)
+	shard.set(key, val)
+}
+
+// SetIfAbsent inserts the given val at the specified key, so long as
+// the specified key does not already exist in the map. A bool is returned
+// indicating whether or not the insert took place.
+func (c *concurrentMap[KEY, VAL]) SetIfAbsent(key KEY, val VAL) bool {
+	shard := c.getShard(key)
+	return shard.setIfAbsent(key, val)
+}
+
+// SetIfPresent inserts the given val at the specified key, so long as
+// the specified key already exists in the map. A bool is returned
+// indicating whether or not the insert took place.
+func (c *concurrentMap[KEY, VAL]) SetIfPresent(key KEY, val VAL) bool {
+	shard := c.getShard(key)
+	return shard.setIfPresent(key, val)
+}
+
+// Len returns the number of keys in the map.
+func (c *concurrentMap[KEY, VAL]) Len() int {
+	size := 0
+	for _, s := range c.shards {
+		if s == nil {
+			continue
+		}
+		s.mutex.RLock()
+		size += len(s.Contents)
+		s.mutex.RUnlock()
+	}
+	return size
+}
+
+// Data returns the contents of all ConcurrentMap shards as a map[KEY]VAL.
+func (c *concurrentMap[KEY, VAL]) Data() map[KEY]VAL {
+	m := make(map[KEY]VAL, c.Len())
+	for _, shard := range c.shards {
+		shard.mutex.RLock()
+		for key, val := range shard.Contents {
+			m[key] = val
+		}
+		shard.mutex.RUnlock()
+	}
+	return m
+}
+
+// Remove deletes the element with the specified key from the
+// map, if it exists. A boolean is returned indicating whether
+// or not any deletion was necessary.
+func (c *concurrentMap[KEY, VAL]) Remove(key KEY) bool {
+	shard := c.getShard(key)
+	return shard.remove(key)
+}
+
+// https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function
+func fnv1a[KEY comparable](key KEY) uint64 {
+	const fnvOffsetBasis = uint64(0xcbf29ce484222325)
+	const fnvPrime = uint64(0x100000001b3)
+
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(key); err != nil {
+		panic(err)
+	}
+
+	hash := fnvOffsetBasis
+	for _, b := range buf.Bytes() {
+		hash ^= uint64(b)
+		hash *= fnvPrime
+	}
+	return hash
+}
 
 type shard[KEY comparable, VAL any] struct {
 	Contents map[KEY]VAL
@@ -36,132 +140,26 @@ func (s *shard[KEY, VAL]) set(k KEY, v VAL) {
 	s.Contents[k] = v
 }
 
-func (s *shard[KEY, VAL]) setIfAbsent(k KEY, v VAL) {
+func (s *shard[KEY, VAL]) setIfAbsent(k KEY, v VAL) bool {
 	if _, ok := s.at(k); !ok {
 		s.set(k, v)
-		return
+		return true
 	}
+	return false
 }
 
-func (s *shard[KEY, VAL]) setIfPresent(k KEY, v VAL) {
+func (s *shard[KEY, VAL]) setIfPresent(k KEY, v VAL) bool {
 	if _, ok := s.at(k); !ok {
-		return
+		return false
 	}
 	s.set(k, v)
+	return true
 }
 
-func (s *shard[_, VAL]) forEach(f func(v VAL) error, exitOnError bool) error {
+func (s *shard[KEY, VAL]) remove(k KEY) bool {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	var errs error = nil
-	for i := range s.Contents {
-		if err := f(s.Contents[i]); err != nil {
-			if exitOnError {
-				return err
-			}
-			if errs == nil {
-				errs = err
-			} else {
-				errs = fmt.Errorf("%w : %s", errs, err)
-			}
-		}
-	}
-	return errs
-}
-
-// ConcurrentMap is a sharded, thread-safe mapping of keys to values.
-type ConcurrentMap[KEY comparable, VAL any] struct {
-	shards [shardCount]*shard[KEY, VAL]
-}
-
-// NewConcurrentMap creates a new ConcurrentMap.
-func NewConcurrentMap[KEY comparable, VAL any]() *ConcurrentMap[KEY, VAL] {
-	c := ConcurrentMap[KEY, VAL]{
-		shards: [shardCount]*shard[KEY, VAL]{},
-	}
-	for i := range c.shards {
-		c.shards[i] = newShard[KEY, VAL]()
-	}
-	return &c
-}
-
-func (c ConcurrentMap[KEY, VAL]) getShard(key KEY) *shard[KEY, VAL] {
-	return c.shards[fnv1a(key)%uint64(shardCount)]
-}
-
-// At returns the value for the given key.
-func (c *ConcurrentMap[KEY, VAL]) At(key KEY) (VAL, bool) {
-	shard := c.getShard(key)
-	return shard.at(key)
-}
-
-// Set inserts the given val at the specified key, or overrides the
-// current value for that key if there is one.
-func (c *ConcurrentMap[KEY, VAL]) Set(key KEY, val VAL) {
-	shard := c.getShard(key)
-	shard.set(key, val)
-}
-
-func (c *ConcurrentMap[KEY, VAL]) SetIfAbsent(key KEY, val VAL) {
-	shard := c.getShard(key)
-	shard.setIfAbsent(key, val)
-}
-
-func (c *ConcurrentMap[KEY, VAL]) SetIfPresent(key KEY, val VAL) {
-	shard := c.getShard(key)
-	shard.setIfPresent(key, val)
-}
-
-// Len returns the number of keys in the map.
-func (c *ConcurrentMap[KEY, VAL]) Len() int {
-	size := 0
-	for _, s := range c.shards {
-		if s == nil {
-			continue
-		}
-		s.mutex.RLock()
-		size += len(s.Contents)
-		s.mutex.RUnlock()
-	}
-	return size
-}
-
-// ForEach runs the provided function for each element in the ConcurrentMap.
-// If exitOnError is true, then this function exits as soon as an error is encountered.
-// Otherwise, execution continues until the provided function is run on every element
-// in the map, wrapping any errors encountered in the process.
-func (c *ConcurrentMap[_, VAL]) ForEach(f func(val VAL) error, exitOnError bool) error {
-	var errs error = nil
-	for _, s := range c.shards {
-		if err := s.forEach(f, exitOnError); err != nil {
-			if exitOnError {
-				return err
-			}
-			if errs == nil {
-				errs = err
-			} else {
-				errs = fmt.Errorf("%w : %s", errs, err)
-			}
-		}
-	}
-	return errs
-}
-
-// https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function
-func fnv1a[KEY comparable](key KEY) uint64 {
-	const fnvOffsetBasis = uint64(0xcbf29ce484222325)
-	const fnvPrime = uint64(0x100000001b3)
-
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	if err := enc.Encode(key); err != nil {
-		panic(err)
-	}
-
-	hash := fnvOffsetBasis
-	for _, b := range buf.Bytes() {
-		hash ^= uint64(b)
-		hash *= fnvPrime
-	}
-	return hash
+	_, ok := s.Contents[k]
+	delete(s.Contents, k)
+	return ok
 }
---
+++ datastructures/concurrent-map_test.go
@@ -0,0 +1,203 @@
+package datastructures_test
+
+import (
+	"testing"
+
+	"github.com/TannerKvarfordt/ubiquity/datastructures"
+)
+
+func TestConcurrentMap(t *testing.T) {
+	m := datastructures.NewConcurrentMap[string, string]()
+	if m.Len() != 0 {
+		t.Error()
+	}
+	if _, ok := m.At(""); ok {
+		t.Error()
+	}
+	if m.Len() != 0 {
+		t.Error()
+	}
+
+	const (
+		key1 = "SLATFATF"
+		val1 = "So long, and thanks for all the fish."
+	)
+	m.Set(key1, val1)
+	if m.Len() != 1 {
+		t.Error()
+	}
+	if val, ok := m.At(key1); ok {
+		if val != val1 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+
+	const (
+		key2 = val1
+		val2 = key1
+	)
+	m.Set(key2, val2)
+	if m.Len() != 2 {
+		t.Error()
+	}
+	if val, ok := m.At(key2); ok {
+		if val != val2 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+}
+
+func TestConcurrentMapSetIfPresent(t *testing.T) {
+	m := datastructures.NewConcurrentMap[string, string]()
+
+	const (
+		key1 = "Douglas Adams"
+		val1 = "So long, and thanks for all the fish."
+		val2 = "Time is an illusion. Lunchtime doubly so."
+	)
+
+	if ok := m.SetIfPresent(key1, val2); ok {
+		t.Error()
+	}
+	if _, ok := m.At(key1); ok {
+		t.Error()
+	}
+	if m.Len() != 0 {
+		t.Error()
+	}
+
+	m.Set(key1, val1)
+	if ok := m.SetIfPresent(key1, val2); !ok {
+		t.Error()
+	}
+	if val, ok := m.At(key1); ok {
+		if val != val2 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+	if m.Len() != 1 {
+		t.Error()
+	}
+}
+
+func TestConcurrentMapSetIfAbsent(t *testing.T) {
+	m := datastructures.NewConcurrentMap[string, string]()
+
+	const (
+		key1 = "Douglas Adams"
+		val1 = "So long, and thanks for all the fish."
+		val2 = "Time is an illusion. Lunchtime doubly so."
+	)
+
+	if ok := m.SetIfAbsent(key1, val1); !ok {
+		t.Error()
+	}
+	if val, ok := m.At(key1); ok {
+		if val != val1 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+	if m.Len() != 1 {
+		t.Error()
+	}
+
+	if ok := m.SetIfAbsent(key1, val2); ok {
+		t.Error()
+	}
+	if val, ok := m.At(key1); ok {
+		if val != val1 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+	if m.Len() != 1 {
+		t.Error()
+	}
+}
+
+func TestConcurrentMapData(t *testing.T) {
+	const (
+		key1 = "SLATFATF"
+		val1 = "So long, and thanks for all the fish."
+
+		key2 = "TIAILDS"
+		val2 = "Time is an illusion. Lunchtime doubly so."
+	)
+	m := datastructures.NewConcurrentMap[string, string]()
+	m.Set(key1, val1)
+	m.Set(key2, val2)
+
+	data := m.Data()
+	if len(data) != 2 {
+		t.Error()
+	}
+	m.Set(val1, key1)
+	if len(data) != 2 {
+		t.Error()
+	}
+
+	if data[key1] != val1 {
+		t.Error()
+	}
+	if data[key2] != val2 {
+		t.Error()
+	}
+}
+
+func TestConcurrentMapRemove(t *testing.T) {
+	const (
+		key1 = "SLATFATF"
+		val1 = "So long, and thanks for all the fish."
+
+		key2 = "TIAILDS"
+		val2 = "Time is an illusion. Lunchtime doubly so."
+	)
+
+	m := datastructures.NewConcurrentMap[string, string]()
+	if removed := m.Remove("foobarbaz"); removed {
+		t.Error()
+	}
+	if m.Len() != 0 {
+		t.Error()
+	}
+
+	m.Set(key1, val1)
+	m.Set(key2, val2)
+	if m.Len() != 2 {
+		t.Error()
+	}
+
+	if removed := m.Remove(key1 + "foobarbaz"); removed {
+		t.Error()
+	}
+
+	if removed := m.Remove(key1); !removed {
+		t.Error()
+	}
+	if m.Len() != 1 {
+		t.Error()
+	}
+	if val, ok := m.At(key2); ok {
+		if val != val2 {
+			t.Error()
+		}
+	} else {
+		t.Error()
+	}
+
+	if removed := m.Remove(key2); !removed {
+		t.Error()
+	}
+	if m.Len() != 0 {
+		t.Error()
+	}
+}
